35208207bf7f540d81032dd07124b7817a270d7c
rubirubsen <rubirubsen@gmail.com>
2024-12-05 12:53:24 +0100

    Add new TCG card display page with interactive hover effects

---
+++ data/tcg.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RZ-TCG Karte mit Schimmer</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="./css/fantamon.css"> <!-- Dein CSS -->
+</head>
+<body>
+<main id=app>
+<a href='https://rubizockt.de' style="color:white" target="_self"><= HOME </a>
+<h1>RubiZockt Sammelkarten, Holo Effect</h1>
+  <section class="cards">
+    <div class="card common animated"></div>
+    <div class="card uncommon animated"></div>
+    <div class="card rare animated"></div>
+    <div class="card urare animated"></div>
+  </section>  
+  <style class="hover"></style>
+  <section class="demo">
+    <div class="card"></div>
+    <span class="operator">+</span>
+    <div class="card"><span>color-dodge</span></div>
+    <span class="operator">+</span>
+    <div class="card"><span>color-dodge</span></div>
+  </section>
+</main>
+
+<script>
+    var x;
+var $cards = $(".card");
+var $style = $(".hover");
+
+$cards
+  .on("mousemove touchmove", function(e) { 
+    // normalise touch/mouse
+    var pos = [e.offsetX,e.offsetY];
+    e.preventDefault();
+    if ( e.type === "touchmove" ) {
+      pos = [ e.touches[0].clientX, e.touches[0].clientY ];
+    }
+    var $card = $(this);
+    // math for mouse position
+    var l = pos[0];
+    var t = pos[1];
+    var h = $card.height();
+    var w = $card.width();
+    var px = Math.abs(Math.floor(100 / w * l)-100);
+    var py = Math.abs(Math.floor(100 / h * t)-100);
+    var pa = (50-px)+(50-py);
+    // math for gradient / background positions
+    var lp = (50+(px - 50)/1.5);
+    var tp = (50+(py - 50)/1.5);
+    var px_spark = (50+(px - 50)/7);
+    var py_spark = (50+(py - 50)/7);
+    var p_opc = 20+(Math.abs(pa)*1.5);
+    var ty = ((tp - 50)/2) * -1;
+    var tx = ((lp - 50)/1.5) * .5;
+    // css to apply for active card
+    var grad_pos = `background-position: ${lp}% ${tp}%;`
+    var sprk_pos = `background-position: ${px_spark}% ${py_spark}%;`
+    var opc = `opacity: ${p_opc/100};`
+    var tf = `transform: rotateX(${ty}deg) rotateY(${tx}deg)`
+    // need to use a <style> tag for psuedo elements
+    var style = `
+      .card:hover:before { ${grad_pos} }  /* gradient */
+      .card:hover:after { ${sprk_pos} ${opc} }   /* sparkles */ 
+    `
+    // set / apply css class and style
+    $cards.removeClass("active");
+    $card.removeClass("animated");
+    $card.attr( "style", tf );
+    $style.html(style);
+    if ( e.type === "touchmove" ) {
+      return false; 
+    }
+    clearTimeout(x);
+  }).on("mouseout touchend touchcancel", function() {
+    // remove css, apply custom animation on end
+    var $card = $(this);
+    $style.html("");
+    $card.removeAttr("style");
+    x = setTimeout(function() {
+      $card.addClass("animated");
+    },2500);
+  });
+  </script>
